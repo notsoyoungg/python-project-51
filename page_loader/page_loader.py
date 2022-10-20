6432fd5b9e051d1da473827377506c510ded4fea
notsoyoung2 <p.sergey1997@mail.ru>
2022-10-20 16:55:07 +0600

    fix page_loader.py

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -24,7 +24,6 @@ bar = Bar('Processing', max=100500)
 def make_request(link):
     r = requests.get(link)
     logger.debug(f'Response from server: {r}')
-    r.raise_for_status()
     logger.debug(r.raise_for_status())
     return r
 
