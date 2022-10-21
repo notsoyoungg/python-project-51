bd6f171bbc5b27d1247769c108ae9432ce26a716
notsoyoung2 <p.sergey1997@mail.ru>
2022-10-21 12:33:20 +0600

    fix page_loader.py

--- page_loader/logger.py
+++ page_loader/logger.py
@@ -4,7 +4,7 @@ import sys
 
 
 logger = logging.getLogger('logger')
-handler = StreamHandler(stream=sys.stderr)
+handler = StreamHandler(stream=sys.stdout)
 handler.setFormatter(Formatter(fmt='[%(asctime)s: %(levelname)s] %(message)s'))
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
